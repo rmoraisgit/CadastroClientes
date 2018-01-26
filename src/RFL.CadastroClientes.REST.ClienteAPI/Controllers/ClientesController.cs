cbb27e46948054cd6ac3412809f4c7127cbb6109
Rafael <rafamorais.br@live.com>
2018-01-26 19:23:52 -0200

    Adicionado WebAPI na aplicação, retornando informações de Clientes em formato Json e Xml

---
+++ src/RFL.CadastroClientes.REST.ClienteAPI/Controllers/ClientesController.cs
@@ -0,0 +1,48 @@
+﻿using RFL.CadastroClientes.Application.Interfaces;
+using RFL.CadastroClientes.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RFL.CadastroClientes.REST.ClienteAPI.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        private readonly IClienteAppService _IClienteAppService;
+
+        public ClientesController(IClienteAppService IClienteAppService)
+        {
+            _IClienteAppService = IClienteAppService;
+        }
+
+        // GET: api/Clientes
+        public IEnumerable<ClienteViewModel> Get()
+        {
+            return _IClienteAppService.BuscarTodos();
+        }
+
+        // GET: api/Clientes/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Clientes
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Clientes/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Clientes/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/RFL.CadastroClientes.REST.ClienteAPI/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using RFL.CadastroClientes.Application.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace RFL.CadastroClientes.REST.ClienteAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AutoMapperConfig.RegisterMappings();
+        }
+    }
+}
